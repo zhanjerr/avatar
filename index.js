6088cc4e4eb62eee64b5998c4d7ec3c1b5cadba6
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-10 16:51:59 +0000

    printed things in chunks

---
+++ index.js
@@ -0,0 +1,29 @@
+var https = require("https");
+
+getAndPrintHTMLChunks();
+
+function getAndPrintHTMLChunks() {
+
+  var requestOptions ={
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
+  };
+
+  https.get(requestOptions, (response) =>{
+    //set encoding
+    response.setEncoding('utf8');
+    // console.log(response);
+    //response on data
+    response.on('data', (data) => {
+      console.log(data + "\n");
+    });
+    //response on error
+    response.on('error', (error) => {
+      console.log("Error: " + error);
+    });
+    //response on end
+    response.on('end', () => {
+      console.log("I've reached the end!");
+    });
+  });
+}
